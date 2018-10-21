e727866fa14b08902f6a6fa618c2738ea53c476a
Fatih Koç <mail@fatihkoc.net>
2018-10-21 16:34:27 +0300

    hello-world

---
+++ fatihkocnet/career/admin.py
@@ -0,0 +1,7 @@
+# Register your models here.
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Project
+
+admin.site.register(Project)
---
+++ fatihkocnet/fatihkocnet/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fatihkocnet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fatihkocnet.settings')
+
+application = get_wsgi_application()
